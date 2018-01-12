0b86ea76002e64b32e384fe67c9015e8e3abf80d
Chris Moreton <chris@netsensia.com>
2018-01-12 23:03:17 +0000

    Binance and Coinbase - needs refactoring

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,30 @@
 */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+
+    $api = new Binance\API(getenv('BINANCE_API_KEY'), getenv('BINANCE_API_SECRET'));
+    $ticker = $api->prices();
+
+    $binanceBalances = array_filter(
+                    $api->balances($ticker),
+                    function ($balance) {
+                        return $balance['btcTotal'] > 0;
+                    }
+                );
+
+    $btcTotal = 0;
+    foreach ($binanceBalances as $balance) {
+        $btcTotal += $balance['btcTotal'];
+    }
+
+    $coinbaseConfiguration = Coinbase\Wallet\Configuration::apiKey(getenv('COINBASE_API_KEY'), getenv('COINBASE_API_SECRET'));
+    $coinbaseClient = Coinbase\Wallet\Client::create($coinbaseConfiguration);
+
+    $exchangeRates = $coinbaseClient->getExchangeRates(['currency' => 'BTC']);
+
+    return [
+        'usd_value' => $btcTotal * $exchangeRates['rates']['USD'],
+        'gbp_value' => $btcTotal * $exchangeRates['rates']['GBP'],
+        'binance' => $binanceBalances,
+    ];
 });
