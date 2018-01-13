2a89150f97a0805d8838e283e0967c69db254c08
Chris Moreton <chris@netsensia.com>
2018-01-13 20:18:20 +0000

    Added BlockCypher balances - still needs refactoring

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,45 @@
 |
 */
 
+use BlockCypher\Auth\SimpleTokenCredential;
+use BlockCypher\Rest\ApiContext;
+use BlockCypher\Client\AddressClient;
+
+function getBalance($currency) {
+
+    $token = getenv('BLOCKCYPHER_TOKEN');
+    
+    $config = array(
+        'mode' => 'sandbox',
+        'log.LogEnabled' => true,
+        'log.FileName' => '/home/vagrant/Code/cryptocurrency-portfolio-api/storage/logs/BlockCypher.log',
+        'log.LogLevel' => 'ERROR', // PLEASE USE 'INFO' LEVEL FOR LOGGING IN LIVE ENVIRONMENTS
+        'validation.level' => 'log',
+    );
+
+    $apiContext = ApiContext::create(
+        'main', strtolower($currency), 'v1',
+        new SimpleTokenCredential($token),
+        $config
+    );
+
+    try {
+        $addressClient = new AddressClient($apiContext);
+        $address = $addressClient->get(getenv(strtoupper($currency) . '_ADDRESS'));
+    } catch (Exception $e) {
+        dd($e->getMessage());
+    }
+
+    return $address->getBalance();
+}
+
 $router->get('/', function () use ($router) {
+
+    $wallets = [];
+    $currencies = ['ETH'];
+    foreach ($currencies as $currency) {
+        $wallets[strtoupper($currency)] = getBalance($currency);
+    }
 
     $api = new Binance\API(getenv('BINANCE_API_KEY'), getenv('BINANCE_API_SECRET'));
     $ticker = $api->prices();
@@ -33,9 +71,14 @@ $router->get('/', function () use ($router) {
 
     $exchangeRates = $coinbaseClient->getExchangeRates(['currency' => 'BTC']);
 
+    $coinbaseAccount = $coinbaseClient->getPrimaryAccount();
+    $coinbaseDeposits = $coinbaseClient->getAccountDeposits($coinbaseAccount);
+
     return [
         'usd_value' => $btcTotal * $exchangeRates['rates']['USD'],
         'gbp_value' => $btcTotal * $exchangeRates['rates']['GBP'],
         'binance' => $binanceBalances,
+        'coinbase' => $coinbaseDeposits,
+        'wallets' => $wallets,
     ];
 });
